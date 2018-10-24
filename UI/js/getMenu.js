e5054a0021b14d62da70a426ffa87ebc4b992651
okellogabrielinnocent@gmail.com <okellogabrielinnocent@gmail.com>
2018-10-24 12:19:27 +0300

    Create js for viewing avialble menu [Delivers #161360189]

--- UI/js/getMenu.js
+++ UI/js/getMenu.js
@@ -1,24 +1,39 @@
-document.getElementById('menu').addEventListener('click', loadMenu);
+document.getElementById('avialable_menu').addEventListener('load', loadMenu);
+let user_userid = localStorage.getItem('Token');
 
 function loadMenu() {
-    fetch('http://127.0.0.1:5000/API/v1/menu')
+    fetch(`http://127.0.0.1:5000/API/v1/menu`, {
+        method:'GET',
+        // pass in the header so that one can ge the token
+        headers:{'Accept': 'application/json, text/plain, */*',
+                "Content-Type": "application/json",
+                "Authorization": "Bearer "+ user_userid}
+    })
     .then(function(response){
-         return response.json();
+        // console.log(response);
+        return response.json();
+         
     })
-    .then(function(menus){
-         let html = '';
-
-         menus.forEach(function(menu){
-              html += `
-                   <li>
-                       ${menu.description}
-                       ${menu.price}
-                   </li>
-              `;
-         });
-         document.getElementById('result').innerHTML = html;
-    })
-    .catch(function(error){
-         console.log(error);
-    })
+    .then(data=>{
+        // console.log(data);
+        let menus = data['Avialable menu'];
+        
+        let htmlInfo = `
+        `;
+        // iterate through returned array by element and key
+        menus.forEach((element,key) => {
+        htmlInfo += `
+            <img src="images/image.webp" alt="image" width="100%">
+            <p>${element.description}</p>
+            <p>shs${element.price}</p>
+            <p><a href="history.html"><button>Order</button></a></p>
+            `;  
+    
+        });
+        document.getElementById('avialable_menu').innerHTML = htmlInfo;
+    
+      })
+      .catch(function(error){
+            console.log(error);
+        })
 }
