9ca82135c8779706bae5223aeb91983a37d3ec88
okellogabrielinnocent@gmail.com <okellogabrielinnocent@gmail.com>
2018-10-30 10:14:06 +0300

    Remove run from .gitignore file

---
+++ run.py
@@ -0,0 +1,8 @@
+from foodapp import app
+
+if __name__ == '__main__':
+    """calls flask App object
+    Runs this file as the fast module
+    Auto reloads when app is run
+    """
+    app.run(debug=True)
