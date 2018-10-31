c33a0560da49e488539efa725a9a25028f53de69
okellogabrielinnocent@gmail.com <okellogabrielinnocent@gmail.com>
2018-10-31 09:45:36 +0300

    Change database connection for heroku db connection

--- foodapp/database.py
+++ foodapp/database.py
@@ -10,8 +10,8 @@ class Database:
 
     def __init__(self):
         if not app.config['TESTING']:
-            self.con = psycopg2.connect(host="localhost", user="postgres",
-                                        password="moschinogab19", dbname="fastfoodfast")
+            self.con = psycopg2.connect(host="ec2-107-20-211-10.compute-1.amazonaws.com", user="yiglrgdinuufse",
+                                        password="38930676ea20c42391dc9c13c91270f6af8af6c235b2bbb9b7dee05e2041886c", dbname="d2datun196lfag")
             
             self.con.autocommit = True
             cur = self.con.cursor()
@@ -24,8 +24,8 @@ class Database:
             cur.execute(tables.ORDER,)
             self.con.commit()
         else:
-            self.con = psycopg2.connect(host="localhost", user="postgres",
-                                        password="moschinogab19", dbname="tesd_db")
+            self.con = psycopg2.connect(host="ec2-107-20-211-10.compute-1.amazonaws.com", user="yiglrgdinuufse",
+                                        password="38930676ea20c42391dc9c13c91270f6af8af6c235b2bbb9b7dee05e2041886c", dbname="d2datun196lfag")
             self.con.autocommit = True
             cur = self.con.cursor()
             cur.execute(tables.USER,)
@@ -40,7 +40,7 @@ class Database:
     @classmethod
     def testing_db_teardown(cls):
         """method to delete tables after testing"""
-        con = psycopg2.connect(host="localhost", user="postgres",
+        con = psycopg2.connect(host="d2datun196lfag", user="yiglrgdinuufse",
                                password="moschinogab19", dbname="test_db")
         return con
 
